6a9e2f3340cf0088f4aaa39b12ede9235e39a0a1
Richard Stuart <richard.niklas.stuart@gmail.com>
2022-12-05 08:15:12 +0100

    day5 part 1

---
+++ day5/day5.py
@@ -0,0 +1,90 @@
+import sys
+import itertools
+
+def main(input):
+    print("Start")
+    lines = []
+    with open(input) as file:
+        lines = file.readlines()
+    
+    #[H]                 [Z]         [J]
+    #[L]     [W] [B]     [G]         [R]
+    #[R]     [G] [S]     [J] [H]     [Q]
+    #[F]     [N] [T] [J] [P] [R]     [F]
+    #[B]     [C] [M] [R] [Q] [F] [G] [P]
+    #[C] [D] [F] [D] [D] [D] [T] [M] [G]
+    #[J] [C] [J] [J] [C] [L] [Z] [V] [B]
+    #[M] [Z] [H] [P] [N] [W] [P] [L] [C]
+    # 1   2   3   4   5   6   7   8   9 
+
+    #.........Top.......................Bottom
+    stack1 = ['H','L','R','F','B','C','J','M']
+    stack2 = ['D','C','Z']
+    stack3 = ['W','G','N','C','F','J','H']
+    stack4 = ['B','S','T','M','D','J','P']
+    stack5 = ['J','R','D','C','N']
+    stack6 = ['Z','G','J','P','Q','D','L','W']
+    stack7 = ['H','R','F','T','Z','P']
+    stack8 = ['G','M','V','L']
+    stack9 = ['J','R','Q','F','P','G','B','C']
+    stack_map = [['x','x'], stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9]
+
+    #first move
+    #move 3 from 2 to 1
+
+    #parse move into nmbr_of_crates_to_move, from_stack, to_stack
+    #append from_stack to to_stack
+
+    #line = "move 3 from 2 to 1"
+    line1 = "move 3 from 2 to 1"
+    line2 = "move 8 from 6 to 4"
+    line3 = "move 4 from 8 to 2"
+    lines = [line1, line2, line3]
+    #nmbr_of_crates_to_move = int(line.split(" ")[1])
+    #from_stack = int(line.split(" ")[3])
+    #to_stack = int(line.split(" ")[5])
+
+    #print(f"[DEBUG] nmbr_of_crates_to_move: {nmbr_of_crates_to_move}")
+    #print(f"[DEBUG] from_stack: {from_stack}")
+    #print(f"[DEBUG] to_stack: {to_stack}")
+    #print(f"[DEBUG] from_tack: {stack_map[from_stack]}")
+    #print(f"[DEBUG] new to_stack: {stack_map[to_stack]}")
+    #print(f"[DEBUG] stack to move: {stack_map[from_stack][:nmbr_of_crates_to_move]}")
+    #stack_map[to_stack].append(stack_map[from_stack][:nmbr_of_crates_to_move])
+    #stack_map[from_stack] = stack_map[from_stack][nmbr_of_crates_to_move:]
+    #print(f"[DEBUG] new from_tack: {stack_map[from_stack]}")
+    #stack_map[to_stack] = list(itertools.chain.from_iterable(stack_map[to_stack]))
+    #print(f"[DEBUG] new to_stack: {stack_map[to_stack]}")
+
+    #reset lines
+    with open(input) as file:
+        lines = file.readlines()
+    #lines = lines[:1]
+
+    for line in lines:
+        print("----------------")
+        nmbr_of_crates_to_move = int(line.split(" ")[1])
+        from_stack = int(line.split(" ")[3])
+        to_stack = int(line.split(" ")[5])
+        print(f"[DEBUG] nmbr_of_crates_to_move: {nmbr_of_crates_to_move}")
+        print(f"[DEBUG] from_stack: {from_stack}")
+        print(f"[DEBUG] to_stack: {to_stack}")
+        print(f"[DEBUG] old from_stack: {stack_map[from_stack]}")
+        print(f"[DEBUG] old to_stack: {stack_map[to_stack]}")
+        temp_move = stack_map[from_stack][:nmbr_of_crates_to_move]
+        temp_move.reverse()
+        print(f"[DEBUG] stack to move: {temp_move}")
+        #stack_map[to_stack].insert(0,stack_map[from_stack][:nmbr_of_crates_to_move])
+        stack_map[to_stack].insert(0,temp_move)
+        stack_map[from_stack] = stack_map[from_stack][nmbr_of_crates_to_move:]
+        stack_map[to_stack] = list(itertools.chain.from_iterable(stack_map[to_stack]))
+        print(f"[DEBUG] new from_tack: {stack_map[from_stack]}")
+        print(f"[DEBUG] new to_stack: {stack_map[to_stack]}")
+
+    stack_str = ""
+    for stack in stack_map:
+        #print(stack)
+        stack_str += stack[0]
+    print(stack_str)
+if __name__ == "__main__":
+    main(sys.argv[1])
