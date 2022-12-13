e93d1d75cf8f1a7756fe3064d0841351bf1ba835
Richard Stuart <richard.niklas.stuart@gmail.com>
2022-12-13 20:14:47 +0100

    part 2 start

--- day8/day8.py
+++ day8/day8.py
@@ -38,7 +38,7 @@ def main(input):
     
     print("------------")
     for row in rows:
-        print(row)
+        #print(row)
         for x in range(0, len(row)):
             try:
                 cols[x] += row[x]
@@ -50,11 +50,6 @@ def main(input):
                 
     #print(cols2)
                 
-    
-    print("------------")
-    for col in cols:
-        print(col)
-    
 
     horizontal_edges = len(lines[0]) - 1
     vertical_edges = len(lines)
@@ -71,17 +66,17 @@ def main(input):
     up = []
     down = []
     visable_trees = 0
-    print(f"line len {len(lines[0])}")
-    print("------------")
+    #print(f"line len {len(lines[0])}")
+    #print("------------")
     for y in range(0, len(lines)):
         for x in range(0, len(lines[0]) - 1):
             current_tree_height = lines[y][x]
-            print(f"current tree coord: {x},{y}")
+            #print(f"current tree coord: {x},{y}")
             if x == 0 or y == 0 or x == len(lines[0]) - 2 or y == len(lines) - 1: #check if on a vertical edge
-                print(f"[DEBUG] current tree is on an edge: {current_tree_height}")
+                #print(f"[DEBUG] current tree is on an edge: {current_tree_height}")
                 visable_trees += 1
             else:
-                print(f"[DEBUG] current tree: {current_tree_height}")
+                #print(f"[DEBUG] current tree: {current_tree_height}")
                 #get left, right, up, down
                 #left = lines[y][:x]
                 #right = lines[y][x:len(lines[0]) - 1]
@@ -89,16 +84,60 @@ def main(input):
                 right = rows[y][x+1:]
                 up = cols[x][:y]
                 down = cols[x][y+1:]
-                print(f"[DEBUG] left: {left}")
-                print(f"[DEBUG] right: {right}")
-                print(f"[DEBUG] up: {up}")
-                print(f"[DEBUG] down: {down}")
+                #print(f"[DEBUG] left: {left}")
+                #print(f"[DEBUG] right: {right}")
+                #print(f"[DEBUG] up: {up}")
+                #print(f"[DEBUG] down: {down}")
                 if check_if_tree_is_higher(current_tree_height, left, right, down, up):
                     visable_trees += 1
-        print("------------")
-    print(f"[DEBUG] visable trees: {visable_trees}")
+        #print("------------")
+    print(f"[DEBUG] Part 1 visable trees: {visable_trees}")
 
-    print(check_if_tree_is_higher(5,[2],[5,1,2],[0],[5,3,3]))
+    best_scenic_score = 0
+    for y in range(0, len(lines)):
+        for x in range(0, len(lines[0]) - 1):
+            print(f"current tree coord: {x},{y}")
+            current_tree_height = lines[y][x]
+            left = rows[y][:x]
+            right = rows[y][x+1:]
+            up = cols[x][:y]
+            down = cols[x][y+1:]
+            current_scenic_score = scenic_score(current_tree_height, left, right, up, down)
+            print(f"[DEBUG] current scenic score: {current_scenic_score}")
+            if current_scenic_score  > best_scenic_score:
+                best_scenic_score = current_scenic_score
+    print(f"[DEBUG] Part 2 best scenic score : {best_scenic_score}")
+
+def scenic_score(current_tree, left: list, right: list, up: list, down: list):
+    left_score = 0
+    right_score = 0
+    up_score = 0
+    down_score = 0
+
+    #left.reverse()
+    for x in range(0,len(left)):
+        if current_tree > x:
+            left_score += 1
+        else:
+            break
+    for x in range(0,len(right)):
+        if current_tree > x:
+            right_score += 1
+        else:
+            break
+    #up.reverse()
+    for x in range(0,len(up)):
+        if current_tree > x:
+            up_score += 1
+        else:
+            break
+    for x in range(0,len(down)):
+        if current_tree > x:
+            down_score += 1
+        else:
+            break
+
+    return left_score * right_score * up_score * down_score
 
 def check_if_tree_is_higher(current_tree, left, right, up, down):
     if current_tree > max(left) or current_tree > max(right) or current_tree > max(up) or current_tree > max(down):
