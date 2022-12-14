473412bc36b2a4239021d8281e27b600b26a9bec
Richard Stuart <richard.niklas.stuart@gmail.com>
2022-12-14 21:26:55 +0100

    continue day 14 part 1

--- day14/day14.py
+++ day14/day14.py
@@ -15,34 +15,98 @@ def main(input):
     with open(input) as file:
         lines = file.readlines()
     
-    smallest_x = 0
-    biggest_x = 0
-    biggest_y = 0
-    
-    start_smallest_x = int(lines[0].split(" ")[0].split(",")[0])
-    print(start_smallest_x)
+    global sep
+    sep = [" ", "," ," -> "]
+
     line = "498,4 -> 498,6 -> 496,6"
+    #rint(rock_formation_to_coords(line))
     
+    rock_coords = []
     for line in lines:
-        smallest_x, biggest_x, biggest_y = find_smallest_and_biggest_rock_coord_from_formation(line, start_smallest_x)
-    #print(find_smallest_and_biggest_rock_coord_from_formation(line, start_smallest_x))
-    print(f"[DEBUG] Map corners: {smallest_x, 0}, {biggest_x, 0}, {0, biggest_y}, {biggest_x, biggest_y}")
-def find_smallest_and_biggest_rock_coord_from_formation(formation: str, old_smallest_x, old_biggest_x, old_biggest_y):
-    sep = [" ", ","]
+        #smallest_x, biggest_x, biggest_y = find_smallest_and_biggest_rock_coord_from_formation(line, start_smallest_x)
+        #print(rock_formation_to_coords(line))
+        for rock in rock_formation_to_coords(line):
+            rock = rock.strip()
+            if not rock in rock_coords:
+                rock_coords.append(rock)
+    print(rock_coords)
+    print(len(rock_coords))
+    old_smallest_x = int(rock_coords[0].split(",")[0])
+    old_biggest_x = old_smallest_x
+    old_biggest_y = int(rock_coords[0].split(",")[1])
+    #print(old_smallest_x, old_biggest_x, old_biggest_y)
+
     smallest_x = old_smallest_x
     biggest_x = old_biggest_x
     biggest_y = old_biggest_y
-    split_formation = formation.split(sep[0])
-    for split in split_formation:
-        if not split == "->":
-            rock_coord = split.split(sep[1])
-            if int(rock_coord[0]) < smallest_x:
-                smallest_x = int(rock_coord[0])
-            if int(rock_coord[0]) > biggest_x:
-                biggest_x = int(rock_coord[0])
-            if int(rock_coord[1]) > biggest_y:
-                biggest_y = int(rock_coord[1])
+    for rock in rock_coords:
+        smallest_x, biggest_x, biggest_y = find_smallest_and_biggest_rock_coord_from_all_rocks(rock, smallest_x, biggest_x, biggest_y)
+    print(f"[DEBUG] Map corners: {smallest_x, 0}, {biggest_x, 0}, {0, biggest_y}, {biggest_x, biggest_y}")
+    sand_fill = 0
+
+
+def find_smallest_and_biggest_rock_coord_from_all_rocks(rock: str, old_smallest_x, old_biggest_x, old_biggest_y):
+    x = int(rock.split(sep[1])[0])
+    y = int(rock.split(sep[1])[1])
+    smallest_x = 0
+    biggest_x = 0
+    biggest_y = 0
+    if x < old_smallest_x:
+        smallest_x = x
+    else:
+        smallest_x = old_smallest_x
+    if x > old_biggest_x:
+        biggest_x = x
+    else:
+        biggest_x = old_biggest_x
+    if y > old_biggest_y:
+        biggest_y = y
+    else:
+        biggest_y = old_biggest_y
     return smallest_x, biggest_x, biggest_y
+
+def rock_formation_to_coords(formation: str):
+    rocks = formation.split(sep[2])
+    print(f"rocks: {rocks}")
+    rocks_to_add = []
+    for i in range(0,len(rocks)):
+        current_rock = rocks[i]
+        try:
+            next_rock = rocks[i + 1]
+        except IndexError:
+            rocks_to_add.append(current_rock)
+            break
+        #check direction to fill in rocks to rock_coord
+        current_rock_x = int(current_rock.split(sep[1])[0])
+        next_rock_x = int(next_rock.split(sep[1])[0])
+        current_rock_y = int(current_rock.split(sep[1])[1])
+        next_rock_y = int(next_rock.split(sep[1])[1])
+        #print(f"current rock: {current_rock_x},{current_rock_y}")
+        #print(f"next rock: {next_rock_x},{next_rock_y}")
+        diff_x = next_rock_x - current_rock_x
+        diff_y = next_rock_y - current_rock_y
+        if diff_x != 0:
+            for j in range(0,abs(diff_x)):
+                if diff_x > 0:
+                    temp_rock = f"{current_rock_x+j},{current_rock_y}"
+                    #print(f"rock to add in positive x direction: {temp_rock}")
+                    rocks_to_add.append(temp_rock)
+                else:
+                    temp_rock = f"{current_rock_x-j},{current_rock_y}"
+                    #print(f"rock to add in negative x direction: {temp_rock}")
+                    rocks_to_add.append(temp_rock)
+        else:
+            for j in range(0,abs(diff_y)):
+                if diff_y > 0:
+                    temp_rock = f"{current_rock_x},{current_rock_y+j}"
+                    #print(f"rock to add in positive y direction: {temp_rock}")
+                    rocks_to_add.append(temp_rock)
+                else:
+                    temp_rock = f"{current_rock_x},{current_rock_y-j}"
+                    #print(f"rock to add in negative y direction: {temp_rock}")
+                    rocks_to_add.append(temp_rock)
+    print(f"rocks to add: {rocks_to_add}")
+    return rocks_to_add
 
 if __name__ == "__main__":
     main(sys.argv[1])
