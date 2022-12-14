46d9297bdc9af2e5d3eab62db0a3ff6416fe9293
Stuart <richard.stuart@altramotion.com>
2022-12-14 12:18:13 +0100

    continue part 1

--- day14/day14.py
+++ day14/day14.py
@@ -15,23 +15,34 @@ def main(input):
     with open(input) as file:
         lines = file.readlines()
     
-    line = "498,4 -> 498,6 -> 496,6"
-    print(find_smallest_and_biggest_rock_coord_from_formation(line))
-def find_smallest_and_biggest_rock_coord_from_formation(formation: str):
-    sep = [" ", ","]
     smallest_x = 0
-    smallest_y = 0
     biggest_x = 0
     biggest_y = 0
+    
+    start_smallest_x = int(lines[0].split(" ")[0].split(",")[0])
+    print(start_smallest_x)
+    line = "498,4 -> 498,6 -> 496,6"
+    
+    for line in lines:
+        smallest_x, biggest_x, biggest_y = find_smallest_and_biggest_rock_coord_from_formation(line, start_smallest_x)
+    #print(find_smallest_and_biggest_rock_coord_from_formation(line, start_smallest_x))
+    print(f"[DEBUG] Map corners: {smallest_x, 0}, {biggest_x, 0}, {0, biggest_y}, {biggest_x, biggest_y}")
+def find_smallest_and_biggest_rock_coord_from_formation(formation: str, old_smallest_x, old_biggest_x, old_biggest_y):
+    sep = [" ", ","]
+    smallest_x = old_smallest_x
+    biggest_x = old_biggest_x
+    biggest_y = old_biggest_y
     split_formation = formation.split(sep[0])
     for split in split_formation:
         if not split == "->":
             rock_coord = split.split(sep[1])
+            if int(rock_coord[0]) < smallest_x:
+                smallest_x = int(rock_coord[0])
             if int(rock_coord[0]) > biggest_x:
                 biggest_x = int(rock_coord[0])
             if int(rock_coord[1]) > biggest_y:
                 biggest_y = int(rock_coord[1])
-    return biggest_x, biggest_y
+    return smallest_x, biggest_x, biggest_y
 
 if __name__ == "__main__":
     main(sys.argv[1])
